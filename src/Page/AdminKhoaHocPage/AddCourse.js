e3f4900ce5de6d14831a0185668498fbb07dd9b6
vanlam <vanlam1412@gmail.com>
2023-05-06 16:59:08 +0700

    update

--- src/Page/AdminKhoaHocPage/AddCourse.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AddCourse() {
-  return (
-    <div>AddCourse</div>
-  )
-}
--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,14 +1,10 @@
 import AdminLayout from "../Layout/AdminLayout";
 
-import AddCourse from "../Page/AdminKhoaHocPage/AddCourse";
-import AdminKhoaHocPage from "../Page/AdminKhoaHocPage/AdminKhoaHocPage";
-import AdminUserPage from "../Page/AdminUserPage/AdminUserPage";
-
-
 import AdminCoursePage from "../Page/AdminCoursePage/AdminCoursePage";
 import UpdateCourse from "../Page/AdminCoursePage/UpdateCourse";
 import AdminUsersPage from "../Page/AdminUserPage/AdminUserPage";
 import LoginPage from "../Page/LoginPage/LoginPage";
+import AddCourse from "../Page/AdminCoursePage/AddCourse";
 
 
 export const adminRoute = [
@@ -39,7 +35,7 @@ export const adminRoute = [
 
     {
         url: "/admin-khoahoc",
-        component: <AdminLayout Componet={AdminKhoaHocPage}></AdminLayout>
+        component: <AdminLayout Componet={AdminCoursePage}></AdminLayout>
     },
 
     {
